3a2c9fdd0472b9d213d1452d4cfa88eeece7e250
dropkicksam <srfhaddaway@gmail.com>
2015-11-12 00:42:16 +0000

    redirect new events to detail page

--- controllers/events.js
+++ controllers/events.js
@@ -99,6 +99,7 @@ function saveEvent(request, response){
   
   if (contextData.errors.length === 0) {
     var newEvent = {
+      id: events.all.length,
       title: request.body.title,
       location: request.body.location,
       image: request.body.image,
@@ -106,7 +107,7 @@ function saveEvent(request, response){
       attending: []
     };
     events.all.push(newEvent);
-    response.redirect('/events');
+    response.redirect('/events/' + String(events.all.length - 1));
   }else{
     response.render('create-event.html', contextData);
   }
